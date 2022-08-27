aa4fe0c3c9250049985d5a10a45d0406c54c5d0c
seltamouni <https://github.com/seltamouni>
2022-08-27 16:39:23 +0100

    add static folder

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
+from itertools import product
+from msilib.schema import ListView
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Product
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
