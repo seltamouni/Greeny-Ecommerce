122e441fe3c9b06e2905d832f36d0955c7b3ea98
seltamouni <https://github.com/seltamouni>
2022-08-27 21:33:58 +0100

    pruduct_list+product_detail

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from itertools import product
 from msilib.schema import ListView
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Product
 # Create your views here.
 
 class ProductList(ListView):
     model = Product
+
+class ProductDetail(DetailView):
+    model = Product    
