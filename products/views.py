8650c3b33927b3a1ba0887f6d709618c3fa21918
seltamouni <https://github.com/seltamouni>
2022-08-28 06:18:46 +0100

    add images to product

--- products/views.py
+++ products/views.py
@@ -2,11 +2,17 @@ from itertools import product
 from msilib.schema import ListView
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Product
+from .models import Product,ProductImages
 # Create your views here.
 
 class ProductList(ListView):
     model = Product
 
 class ProductDetail(DetailView):
-    model = Product    
+    model = Product 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        myproduct = self.get_object()
+        context["images"] = ProductImages.objects.filter(product=myproduct)
+        return context
+       
